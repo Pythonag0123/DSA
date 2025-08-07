c4dd55fed9255651d46b0e89043ed30074781799
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-08-07 09:33:31 +0000

    implementation

--- TREE/binaryTree/binaryTree.java
+++ TREE/binaryTree/binaryTree.java
@@ -1,3 +1,4 @@
+package binaryTree;
 import java.util.*;
 public class binaryTree{
     class Node{
--- TREE/binaryTree/binaryTree_client.java
+++ TREE/binaryTree/binaryTree_client.java
@@ -1,3 +1,4 @@
+package binaryTree;
 public class binaryTree_client{
     public static void main(String[] args){
         binaryTree bt=new binaryTree();
