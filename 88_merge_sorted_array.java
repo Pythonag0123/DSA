b0be1ca1f04d05b9fa85e4b9d6a7c37109f9be76
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-08-28 19:37:31 +0530

    Add 88_merge_sorted_array.java - LeetCode Problem 88

---
+++ 88_merge_sorted_array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len=nums1.length;
+        int i=0;
+        while(i<n){
+            nums1[m]=nums2[i];
+            m++;
+            i++;
+            
+        }
+        Arrays.sort(nums1);
+        
+    }
+}
