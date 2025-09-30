b5fc2c02641100304c9f2848bef362085a7676d5
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-09-30 09:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (20.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,48 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 1 && nums[0] == target) {
-            return 0;
-        } else if (nums.length == 1 && nums[0] != target) {
-            return -1;
-        }
+        int low = 0, high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        int y = findPeak(nums);
-        int c = se(nums, target, 0, y - 1);     
-        int d = se(nums, target, y, nums.length - 1);
+            if (nums[mid] == target) return mid;
 
-        if (c != -1) return c;
-        else return d;
-    }
-
-    public int se(int[] nums, int target, int s, int e) {
-        int ans = -1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                return ans;
-            } else if (nums[mid] < target) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+            // Left half sorted
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right half sorted
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return ans;
-    }
-
-    
-    public int findPeak(int[] nums) {
-        int n = nums.length;
-        int lo = 0;
-        int hi = n - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] > nums[hi]) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
-        }
-        return lo;
+        return -1;
     }
 }
