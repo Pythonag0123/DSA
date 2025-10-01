81fa9be83b685945d836439b79ee36d075575eaa
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-02 02:33:10 +0530

    Time: 17 ms (21.58%) | Memory: 55.1 MB (53.30%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n=people.length;
+        int s=0;
+        int e=n-1;
+        int boats=0;
+        while(s<=e){
+            if(people[s]+people[e]<=limit){
+                boats++;
+                s++;
+                e--;
+            }
+            else{
+                boats++;
+                e--;
+
+            }
+
+        }
+        return boats;
+        
+    }
+}
