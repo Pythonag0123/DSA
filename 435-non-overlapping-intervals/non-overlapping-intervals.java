e7ae863c52434fc089703ebd04ae2078895cab22
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-02 01:30:32 +0530

    Time: 52 ms (21.35%) | Memory: 73.2 MB (95.68%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+        int n = intervals.length;
+        int count = 0;
+        
+        int prevEnd = intervals[0][1]; // keep first interval
+        
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < prevEnd) {
+                
+                count++;
+            } else {
+                
+                prevEnd = intervals[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
