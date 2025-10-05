eb40ae60b3acf87f04f641a9dae6008cd29e2ecf
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-05 15:21:32 +0530

    Time: 4 ms (38.75%) | Memory: 43.2 MB (73.35%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -2,31 +2,27 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ll=new ArrayList<>();
+        List<Integer>ll=new ArrayList<>();
         solve(candidates,target,0,ans,ll);
         return ans;
 
         
     }
-    public static void solve(int[] arr,int target,int idx,List<List<Integer>> ans,List<Integer> ll){
-        if(target==0){
+    public void solve(int[] arr,int amount,int idx,List<List<Integer>> ans,List<Integer>ll){
+        if(amount==0){
             ans.add(new ArrayList<>(ll));
             return;
         }
-        if(idx==arr.length||target<0){
+        if(idx==arr.length||amount<0){
             return;
         }
         ll.add(arr[idx]);
-        solve(arr,target-arr[idx],idx+1,ans,ll);
+        solve(arr,amount-arr[idx],idx+1,ans,ll);
         ll.remove(ll.size()-1);
         int next=idx+1;
-        while(next<arr.length && arr[idx]==arr[next]){
+        while(next<arr.length && arr[next]==arr[idx]){
             next++;
         }
-        solve(arr,target,next,ans,ll);
-
-        
-
+        solve(arr,amount,next,ans,ll);
     }
-
 }
