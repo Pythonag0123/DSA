9aaefcb74f450425f62cbaae7aa5192f09b5127e
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-05 16:36:41 +0530

    Time: 1 ms (99.96%) | Memory: 43.6 MB (45.30%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,32 +1,24 @@
 class Solution {
-    Set<List<Integer>> set=new HashSet<>();
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ll=new ArrayList<>();
-        solve(nums,0,ans,ll);
-        if(!set.contains(new ArrayList<>())){
-            ans.add(new ArrayList<>());
+    public void backTrack(List<List<Integer>> result, List<Integer> temp ,int nums[], int start)
+    {
+        result.add(new ArrayList<>(temp));
+
+        for(int i = start; i<nums.length; i++)
+        {
+            if(i>start && nums[i]==nums[i-1])
+            continue;
+            
+            temp.add(nums[i]);
+
+            backTrack(result,temp,nums,i+1);
+
+            temp.remove(temp.size()-1);
         }
-        return ans;
-        
-
-        
-
     }
-    public void solve(int[] arr,int idx,List<List<Integer>> ans,List<Integer> ll){
-        if(idx==arr.length){
-            if(!set.contains(ll)){
-                set.add(new ArrayList<>(ll));
-                ans.add(new ArrayList<>(ll));
-            }
-            return;
-        }
-        solve(arr,idx+1,ans,ll);
-        ll.add(arr[idx]);
-        solve(arr,idx+1,ans,ll);
-        ll.remove(ll.size()-1);
-
-
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+        backTrack(resultList,new ArrayList<>(), nums, 0);
+        return resultList;
     }
 }
