544711c29c32999afa77a096f7322c12eb6a9fde
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-14 22:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (79.77%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,14 +1,15 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int len=nums1.length;
-        int i=0;
-        while(i<n){
-            nums1[m]=nums2[i];
-            m++;
-            i++;
-            
-        }
-        Arrays.sort(nums1);
+   public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int pMerge = m + n - 1;
         
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[pMerge--] = nums1[p1--];
+            } else {
+                nums1[pMerge--] = nums2[p2--];
+            }
+        }
     }
 }
