8823ac8dfaf3fca4e0dcf6e5e5f847df762dd300
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-22 22:52:37 +0530

    Time: 1 ms (93.01%) | Memory: 44.5 MB (82.77%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ll=new ArrayList<>();
-
-        helper(nums,ll,ans);
-        return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
         
+        
+        boolean[] used = new boolean[nums.length];
+        
+        helper(nums, ll, ans, used);
+        return ans;
     }
-    public void helper(int[] arr,List<Integer> ll,List<List<Integer>> ans){
-        if(ll.size()==arr.length){
+
+  
+    public void helper(int[] arr, List<Integer> ll, List<List<Integer>> ans, boolean[] used) {
+        if (ll.size() == arr.length) {
             ans.add(new ArrayList<>(ll));
             return;
         }
-        for(int i=0;i<arr.length;i++){
-            if(ll.contains(arr[i])) continue;
-            
-            ll.add(arr[i]);
-            helper(arr,ll,ans);
-            ll.remove(ll.size()-1);
-            
-            
-        }
         
-    
+        for (int i = 0; i < arr.length; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+            
+            used[i] = true; 
+            ll.add(arr[i]);
+            
+         
+            helper(arr, ll, ans, used);
+            
+
+            ll.remove(ll.size() - 1);
+            used[i] = false; 
+        }
     }
 }
