76a19382ffd97ac301796efa11cb4141c61c28ae
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-24 14:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (34.34%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,21 +1,41 @@
 class Solution {
-    int res = 0, empty = 1;
-    public int uniquePathsIII(int[][] g) {
-        int sx = 0, sy = 0;
-        for (int i = 0; i < g.length; i++)
-            for (int j = 0; j < g[0].length; j++) {
-                if (g[i][j] == 0) empty++;
-                else if (g[i][j] == 1) { sx = i; sy = j; }
+    public int uniquePathsIII(int[][] grid) {
+        int zero = 0; // Count the 0's
+        int sx = 0; // starting x index
+        int sy = 0; // starting y index
+        
+        for(int r = 0; r < grid.length; r++){ // r = row
+            for(int c = 0; c < grid[0].length; c++){ // c = column
+                if(grid[r][c] == 0) zero++; // if current cell is 0, count it.
+                else if(grid[r][c] == 1){
+                    sx = r; // starting x co-ordinate
+                    sy = c; // starting y co-ordinate
+                }
             }
-        dfs(g, sx, sy);
-        return res;
+        }
+        return dfs(grid, sx, sy, zero);
     }
-
-    void dfs(int[][] g, int i, int j) {
-        if (i < 0 || j < 0 || i >= g.length || j >= g[0].length || g[i][j] == -1) return;
-        if (g[i][j] == 2) { if (empty == 0) res++; return; }
-        g[i][j] = -1; empty--;
-        dfs(g, i+1, j); dfs(g, i-1, j); dfs(g, i, j+1); dfs(g, i, j-1);
-        g[i][j] = 0; empty++;
+    public int dfs(int grid[][], int x, int y, int zero){
+        // Base Condition
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == -1){
+            return 0;
+        }
+        if(grid[x][y] == 2){
+            return zero == -1 ? 1 : 0; // Why zero = -1, because in above example we have 9 zero's. So, when we reach the final cell we are covering one cell extra then the zero count. 
+            // If that's the case we find the path and return '1' otherwise return '0';
+        }
+        grid[x][y] = -1; // mark the visited cells as -1;
+        zero--; // and reduce the zero by 1
+        
+        int totalPaths = dfs(grid, x + 1, y, zero) + // calculating all the paths available in 4 directions
+            dfs(grid, x - 1, y, zero) + 
+            dfs(grid, x, y + 1, zero) + 
+            dfs(grid, x, y - 1, zero);
+        
+        // Let's say if we are not able to count all the paths. Now we use Backtracking over here
+        grid[x][y] = 0;
+        zero++;
+        
+        return totalPaths; // if we get all the paths, simply return it.
     }
 }
