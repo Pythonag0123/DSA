58aebf5f9e5ffbf8e9b4024294292a94a45eced6
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-10-30 14:21:11 +0530

    Time: 2 ms (99.51%) | Memory: 46.3 MB (16.45%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,35 +1,17 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
-        
-        for (int i = 0; i < n; i++) {
-            left[i] = 1;
-            right[i] = 1;
+        int cnt = 0;
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) candies[i] = 1;
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = Math.max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
         }
-        
-        
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
-            }
-        }
-        
-        
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                right[i] = right[i + 1] + 1;
-            }
-        }
-        
-        
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.max(left[i], right[i]);
-        }
-        
-        return sum;
+        return cnt + candies[n - 1];
     }
 }
