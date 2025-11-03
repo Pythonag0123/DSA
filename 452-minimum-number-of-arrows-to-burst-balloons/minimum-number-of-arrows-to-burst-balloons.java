c0dd7c4899b69a1303873de2453efe43dad629e4
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-11-04 00:16:25 +0530

    Time: 51 ms (98.51%) | Memory: 95.6 MB (5.08%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,32 +1,16 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) return 0;
-
-        
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
-
-        int arrows = 1;
-        int prevSp = points[0][0];
-        int prevEp = points[0][1];
-
-        
-        for (int i = 1; i < points.length; i++) {
-            int currSp = points[i][0];
-            int currEp = points[i][1];
-
-            
-            if (currSp <= prevEp) {
-                prevSp = Math.max(prevSp, currSp);
-                prevEp = Math.min(prevEp, currEp); 
-            } 
-            
-            else {
+        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
+        int arrows=1;
+        int lastBalloon=points[0][1];
+        for(int i=1;i<points.length;i++){
+            if(points[i][0]>lastBalloon){
                 arrows++;
-                prevSp = currSp;
-                prevEp = currEp;
+                lastBalloon=points[i][1];
             }
+            
         }
-
         return arrows;
+        
     }
 }
