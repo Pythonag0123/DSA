33c5d2d3e8e4cb101bea3bfa6603a2851ea8daa5
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-12-03 21:54:18 +0530

    Time: 30 ms (14.50%) | Memory: 58.8 MB (5.18%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp=new int[amount+1][coins.length];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+        int n=amount+1;
+        int m=coins.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=-1;
+            }
         }
-        return ways(amount,coins,0,dp);
+        return ways(amount,0,coins,dp);
         
     }
-    public int ways(int amount,int[]coins,int idx,int[][] dp){
-        int pick=0;
-        int notpick=0;
+    public static int ways(int amount,int idx,int[] coins,int[][] dp){
         if(amount==0) return 1;
         if(idx==coins.length) return 0;
-        if(dp[amount][idx]!=-1){
-            return dp[amount][idx];
-        }
+        if(amount<0) return 0;
+        if(dp[amount][idx]!=-1) return dp[amount][idx];
+        int pick=ways(amount-coins[idx],idx,coins,dp);
 
-
-        if(amount>=coins[idx]){
-            pick+=ways(amount-coins[idx],coins,idx,dp);
-        }
-        notpick+=ways(amount,coins,idx+1,dp);
+        int notpick=ways(amount,idx+1,coins,dp);
         return dp[amount][idx]=pick+notpick;
-        
     }
+
 }
