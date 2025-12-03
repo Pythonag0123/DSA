1109a8f63ec739ec5bbfcaa6b4a0437885a8cff4
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-12-03 11:54:44 +0530

    Time: 7 ms (24.59%) | Memory: 45.6 MB (35.50%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,21 +1,43 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return lcsbu(nums1,nums2);
-        
-    }
-    public static int lcsbu(int[] s1,int[] s2){
-        int n=s1.length;
-        int m=s2.length;
-        int[][] dp=new int[n+1][m+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[][] dp = new int[n][m];
+
+        // Fill dp with -1 for memoization
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                dp[i][j] = -1;
             }
         }
-        return dp[n][m];
+
+        return ans(nums1, nums2, 0, 0, dp);
+    }
+
+    public static int ans(int[] nums1, int[] nums2, int i, int j, int[][] dp){
+
+        // Base case
+        if(i == nums1.length || j == nums2.length){
+            return 0;
+        }
+
+        // Memoized answer
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        // Matching numbers → we can draw a line → count 1
+        if(nums1[i] == nums2[j]){
+            dp[i][j] = 1 + ans(nums1, nums2, i + 1, j + 1, dp);
+        }
+        else{
+            // No match → choose best of skipping from either sequence
+            int skip1 = ans(nums1, nums2, i + 1, j, dp);
+            int skip2 = ans(nums1, nums2, i, j + 1, dp);
+            dp[i][j] = Math.max(skip1, skip2);
+        }
+
+        return dp[i][j];
     }
 }
