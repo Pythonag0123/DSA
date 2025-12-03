5f1161643b385325883216cfe4cf892808aa6754
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2025-12-03 10:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (23.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+
+        // dp[i] = number of ways to reach step i
+
+        if(n == 0) return 1;     // 1 way (stay at ground)
+        if(n == 1) return 1;     // 1 way (1 step)
 
         int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        // Base cases
+        dp[0] = 1;   // ground
+        dp[1] = 1;   // first step
+
+        // Fill dp array from 2 to n
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];  
+            // You can reach i either from:
+            // - i-1 step (take 1 step)
+            // - i-2 step (take 2 steps)
         }
 
-        return dp[n];
+        return dp[n];   // ways to reach nth step
     }
 }
