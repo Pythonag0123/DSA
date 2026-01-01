a80b56161d06004987b4a60f2dabc24cc49abed8
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-01 15:54:49 +0530

    Time: 6 ms (24.63%) | Memory: 51.1 MB (94.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,54 @@
-public class Solution {
-
-    private int n;
-    private int m;
+class Solution {
 
     public int numIslands(char[][] grid) {
-        int count = 0;
-        n = grid.length;
-        if (n == 0) return 0;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+        int islands = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                // new island found
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    islands++;
+                    bfs(grid, i, j, vis);
                 }
-        }    
-        return count;
+            }
+        }
+        return islands;
     }
 
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+    private void bfs(char[][] grid, int sr, int sc, boolean[][] vis) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        vis[sr][sc] = true;
+
+        // 4 directions
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                    grid[nr][nc] == '1' && !vis[nr][nc]) {
+
+                    vis[nr][nc] = true;
+                    q.add(new int[]{nr, nc});
+                }
+            }
+        }
     }
 }
