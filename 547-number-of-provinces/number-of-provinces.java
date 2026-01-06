d17415bf17f546a123091383d0e65a56b5705ad2
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-06 14:06:56 +0530

    Time: 1 ms (86.35%) | Memory: 47.2 MB (61.16%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,60 +1,25 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        return answer(isConnected);
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(isConnected, visited, i);
+                provinces++;
+            }
+        }
+        return provinces;
     }
 
-    public int answer(int[][] mat){
-        int n = mat.length;
+    void dfs(int[][] isConnected, boolean[] visited, int city){
+        visited[city] = true;
 
-        //create graph
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for(int i=1 ;i<=n ;i++){
-            map.put(i, new ArrayList<>());
-        }
-        for(int i=0; i<n ;i++){
-            for(int j=0 ;j<n ;j++){
-                if(i != j && mat[i][j] == 1){
-                    map.get(i+1).add(j+1);
-                    map.get(j+1).add(i+1);
-                }
+        for(int j = 0; j < isConnected.length; j++){
+            if(isConnected[city][j] == 1 && !visited[j]){
+                dfs(isConnected, visited, j);
             }
         }
-
-        //now apply bfs to find no. of components
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> visited = new HashSet<>();
-        int com =0;
-
-        for(int src : map.keySet()){
-            if(visited.contains(src)){
-                continue;
-            }
-            com++;
-            q.add(src);
-
-            while(!q.isEmpty()){
-                // remove
-                int r = q.poll();
-
-                // ignore if visited
-                if(visited.contains(r)){
-                    continue;
-                }
-                // mark visited
-                visited.add(r);
-                // self work
-
-                // add unvisited nbrs
-                for(int nbrs : map.get(r)){
-                    if(!visited.contains(nbrs)){
-                        q.add(nbrs);
-                    }
-                }
-
-            }
-        }
-        return com;
-
-        
     }
 }
