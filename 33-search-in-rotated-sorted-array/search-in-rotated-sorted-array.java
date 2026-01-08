e7d25ade32f414b6c40724d80d4c7a182644481e
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-09 01:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.37%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,33 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+        int low = 0, hi = nums.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (low <= hi) {
+            int mid = low + (hi - low) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            }
+            if (nums[mid] == target) return mid;
 
-            // Left half is sorted
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
+            // left half sorted
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    hi = mid - 1;
                 } else {
-                    start = mid + 1;
+                    low = mid + 1;
                 }
             }
-            // Right half is sorted
+            // right half sorted
             else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
+                if (target > nums[mid] && target <= nums[hi]) {
+                    low = mid + 1;
                 } else {
-                    end = mid - 1;
+                    hi = mid - 1;
                 }
             }
         }
-
-        return -1; // target not found
+        return -1;
     }
 }
