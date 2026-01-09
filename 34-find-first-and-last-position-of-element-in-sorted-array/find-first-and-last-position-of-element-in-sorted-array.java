5cd1da53b8415390456309c18298f6a2dff2b7a1
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-09 13:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (53.67%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        ans[0] = firstPos(nums, target);
-        ans[1] = lastPos(nums, target);
-        return ans;
+        int n=nums.length;
+        int[] pos=new int[2];
+        pos[0]=firstPos(nums,target);
+        pos[1]=lastPos(nums,target);
+        return pos;
     }
-
-    public int firstPos(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
-        int ans = -1;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;        
-                hi = mid - 1;     
-            } 
-            else if (nums[mid] < target) {
-                lo = mid + 1;
-            } 
-            else {
-                hi = mid - 1;
+    public int firstPos(int[] nums,int target){
+        int hi=nums.length-1;
+        int lo=0;
+        int f=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(target==nums[mid]){
+                f=mid;
+                hi=mid-1;
             }
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+
         }
-        return ans;
+        return f;
+
+
     }
-
-    public int lastPos(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
-        int ans = -1;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;        
-                lo = mid + 1;    
-            } 
-            else if (nums[mid] < target) {
-                lo = mid + 1;
-            } 
-            else {
-                hi = mid - 1;
+    public int lastPos(int[] nums,int target){
+        int hi=nums.length-1;
+        int lo=0;
+        int l=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(target==nums[mid]){
+                l=mid;
+                lo=mid+1;
             }
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+
         }
-        return ans;
+        return l;
+
     }
 }
