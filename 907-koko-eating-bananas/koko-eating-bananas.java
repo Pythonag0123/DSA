81ab677dff8aedd4b42ce8c62d12982b40852c18
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-10 16:32:52 +0530

    Time: 6 ms (99.55%) | Memory: 48.6 MB (5.25%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,31 +1,32 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int lo=1;
-        int hi=0;
-        for(int p:piles) hi=Math.max(p,hi);
-        while(lo<hi){
-            int mid=lo+(hi-lo)/2;
-            if(canEat(piles,h,mid)){
-                hi=mid;
-            }
-            else{
-                lo=mid+1;
-            }
+        int lo = 1;
+        int hi = 0;
 
+        for (int p : piles) {
+            hi = Math.max(hi, p);
         }
-        return lo;
 
+        int ans = hi;
 
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
 
-        
+            if (canEat(piles, h, mid)) {
+                ans = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return ans;
     }
-    public boolean canEat(int[] piles,int h,int s){
-        int time=0;
-        for(int p:piles){
-            time+=(p+s-1)/s;
-            if(time>h) return false;
-        }
-        return true;
 
+    public boolean canEat(int[] piles, int h, int speed) {
+        long hours = 0;
+        for (int pile : piles) {
+            hours += (pile + speed - 1) / speed;
+        }
+        return hours <= h;
     }
 }
