3cdcbdaa77faff1a36ff59bd9734b0586daecc53
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-11 16:07:49 +0530

    Time: 8 ms (89.37%) | Memory: 49.3 MB (26.27%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        List<int[]> result=new ArrayList<>();
-        int[] curr=intervals[0];
-        for(int i=1;i<intervals.length;i++){
-            int[] next=intervals[i];
-            if(next[0] <= curr[1]){
-                curr[1]=Math.max(curr[1],next[1]);
+        int n=intervals.length;
+        if(n<=1) return intervals;
+        List<int[]> res=new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=end){
+                end=Math.max(end,intervals[i][1]);
             }
             else{
-                result.add(curr);
-                curr=next;
+                res.add(new int[]{start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
             }
+
         }
-        result.add(curr);
-        return result.toArray(new int[result.size()][]);
+        res.add(new int[]{start,end});
+        return res.toArray(new int[res.size()][]);
         
     }
 }
