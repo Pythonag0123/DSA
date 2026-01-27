c5eaa4114da0c20f8f2f5e89ef15be855e179f00
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-28 00:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (15.24%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,47 @@
+class MyStack {
+    Queue<Integer> q;
+
+    public MyStack() {
+        q=new LinkedList<>();
+        
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        int size=q.size();
+        for(int i=0;i<size-1;i++){
+            q.add(q.poll());
+        }
+        
+    }
+    
+    public int pop() {
+        if(q.isEmpty()) return -1;
+        else{
+            return q.poll();
+        }
+        
+    }
+    
+    public int top() {
+        if(q.isEmpty()) return -1;
+        else{
+            return q.peek();
+        }
+
+        
+    }
+    
+    public boolean empty() {
+        return q.size()==0;        
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
