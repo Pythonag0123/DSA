0c8851129e2f9bf98406750c8f4810b190c383ba
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-01-30 13:44:33 +0530

    Time: 48 ms (17.29%) | Memory: 95.2 MB (11.53%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.java
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxResult(int[] nums, int k) {
         int n=nums.length;
         int max=nums[0];
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
         pq.offer(new int[]{0,nums[0]});
         for(int i=1;i<n;i++){
             while(i-pq.peek()[0]>k){
@@ -13,7 +13,7 @@ class Solution {
             pq.offer(new int[]{i,max});
         }
         return max;
-         
+        
 
         
     }
