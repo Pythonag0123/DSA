62f655ac79c122d8e11dea487207481f4fadf6f7
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-02-03 15:16:48 +0530

    Time: 5 ms (55.47%) | Memory: 48.5 MB (59.94%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -1,35 +1,36 @@
 class Solution {
     public ListNode doubleIt(ListNode head) {
+        head = reverse(head);
 
-        // Step 1: build string (use StringBuilder for speed)
-        StringBuilder sb = new StringBuilder();
-        ListNode a = head;
-
-        while (a != null) {
-            sb.append(a.val);
-            a = a.next;
-        }
-
-        // Step 2: double the number manually (string math)
         int carry = 0;
+        ListNode curr = head;
 
-        for (int i = sb.length() - 1; i >= 0; i--) {
-            int digit = (sb.charAt(i) - '0') * 2 + carry;
-            sb.setCharAt(i, (char) ((digit % 10) + '0'));
-            carry = digit / 10;
-        }
-
-        if (carry > 0) sb.insert(0, carry);
-
-        // Step 3: convert string back to list
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-
-        for (int i = 0; i < sb.length(); i++) {
-            curr.next = new ListNode(sb.charAt(i) - '0');
+        while (curr != null) {
+            int val = curr.val * 2 + carry;
+            curr.val = val % 10;
+            carry = val / 10;
             curr = curr.next;
         }
 
-        return dummy.next;
+        head = reverse(head);
+
+        if (carry > 0) {
+            ListNode node = new ListNode(carry);
+            node.next = head;
+            head = node;
+        }
+
+        return head;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
