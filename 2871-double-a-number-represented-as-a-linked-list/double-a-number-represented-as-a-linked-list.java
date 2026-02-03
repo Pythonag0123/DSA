d0b3895f61fcc375a9369dac9e26f3f0bdc33c86
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-02-03 15:20:22 +0530

    Time: 8 ms (18.92%) | Memory: 48.6 MB (42.08%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -24,13 +24,16 @@ class Solution {
     }
 
     private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
+    // base case
+    if (head == null || head.next == null)
+        return head;
+
+    ListNode newHead = reverse(head.next);
+
+    head.next.next = head; // reverse link
+    head.next = null;     // break old link
+
+    return newHead;
     }
+
 }
