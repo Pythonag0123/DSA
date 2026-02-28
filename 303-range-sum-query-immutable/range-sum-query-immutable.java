aa45a0f0d242611473b61b86fc4dc8498efb7983
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-02-28 13:02:04 +0530

    Time: 8 ms (71.64%) | Memory: 47.5 MB (96.06%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,24 +1,25 @@
 class NumArray {
-    private int[] prefix;  
+
+    int[] prefix;
 
     public NumArray(int[] nums) {
         int n = nums.length;
-        prefix = new int[n];  
-        if(n > 0) {
-            prefix[0] = nums[0];
-            for(int i = 1; i < n; i++){
-                prefix[i] = prefix[i-1] + nums[i];
-            }
+        prefix = new int[n];   // removed int
+
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] + nums[i];
         }
     }
-
+    
     public int sumRange(int left, int right) {
-        
-        if(left == 0) return prefix[right];
+        if(left == 0){
+            return prefix[right];
+        }
         return prefix[right] - prefix[left - 1];
     }
 }
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
