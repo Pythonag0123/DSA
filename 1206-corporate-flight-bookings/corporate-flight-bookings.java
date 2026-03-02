7ed91fb5463efc9def4441638a765404e1fb4515
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-03-02 12:36:53 +0530

    Time: 4 ms (45.96%) | Memory: 84.8 MB (60.31%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] flight = new int[n];
-        int m = bookings.length;
-
-        for(int i=0;i<m;i++){
-            int s = bookings[i][0] - 1; 
-            int e = bookings[i][1];    
-            int se = bookings[i][2];
-
-            flight[s] += se;
-            if(e < n){
-                flight[e] -= se;
+        int[] ans=new int[n];
+        for(int i=0;i<bookings.length;i++){
+            int l=bookings[i][0]-1;
+            int r=bookings[i][1]-1;
+            int val=bookings[i][2];
+            
+            ans[l]+=val;
+            if(r+1<n){
+                ans[r+1]-=val;
             }
+                
+            
         }
-
         for(int i=1;i<n;i++){
-            flight[i] += flight[i-1];
+            ans[i]=ans[i]+ans[i-1];
         }
-        return flight;
+        return ans;
+        
     }
 }
