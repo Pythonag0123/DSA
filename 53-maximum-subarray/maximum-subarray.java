5714abe834afa864c29661d3cb5f5d9ebdf0ad04
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2026-03-04 21:27:38 +0530

    Time: 1 ms (99.76%) | Memory: 77.1 MB (83.45%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int n=nums.length;
-        int max=nums[0];
-        int currMax=nums[0];
+        int ans=nums[0];
+        int sum=nums[0];
         for(int i=1;i<n;i++){
-            currMax=Math.max(nums[i],currMax+nums[i]);
-            max=Math.max(currMax,max);
-
+            sum=Math.max(nums[i],sum+nums[i]);
+            ans=Math.max(sum,ans);
         }
-        return max;
+        return ans;
         
     }
 }
